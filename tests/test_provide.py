a7895ff416f86ef0ae3458a01dc8fe3a11f94533
ivan <ivan.belyaev@ailet.com>
2024-10-27 23:55:15 +0300

    Make injecting more simpler, fix tests

--- tests/test_provide.py
+++ tests/test_provide.py
@@ -6,5 +6,5 @@ from injection import Provide
 @pytest.mark.parametrize("value", [(1,), (object,)])
 def test_provide_returns_self(value):
     provide = Provide[value]
-    assert provide is provide()
+    assert isinstance(provide, Provide)
     assert provide.provider is value
