361ee68e28fda506316521aad3e790f55f5f9811
ivan <ivan.belyaev@ailet.com>
2024-11-17 13:16:06 +0300

    fix bug with call with args

--- src/injection/providers/object.py
+++ src/injection/providers/object.py
@@ -1,4 +1,4 @@
-from typing import TypeVar, cast
+from typing import Any, TypeVar, Union, cast
 
 from injection.providers.base import BaseProvider
 from injection.resolving import resolve_value
@@ -14,3 +14,8 @@ class Object(BaseProvider[T]):
     def _resolve(self) -> T:
         value = cast(T, resolve_value(self._obj))
         return value
+
+    def __call__(self) -> Union[T, Any]:
+        if self._mocks:
+            return self._mocks[-1]
+        return self._resolve()
