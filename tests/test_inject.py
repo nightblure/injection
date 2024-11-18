bc7b55a607e845f7646a5d41e35253b15efc6b9d
Ivan Belyaev <vanobel159@gmail.com>
2024-11-18 23:24:17 +0300

    tests for auto injecting

--- tests/test_inject.py
+++ tests/test_inject.py
@@ -7,8 +7,10 @@ def test_injection_with_args_overriding(container) -> None:
     def _inner(
         arg1: bool,  # noqa: FBT001
         arg2: Service = Provide[container.service],
+        arg3: int = 100,
     ) -> None:
         _ = arg1
+        _ = arg3
         original_obj = container.service()
         assert arg2.a != original_obj.a
         assert arg2.b != original_obj.b
@@ -23,8 +25,10 @@ async def test_injection_with_args_overriding_async(container) -> None:
     async def _inner(
         arg1: bool,  # noqa: FBT001
         arg2: Service = Provide[container.service],
+        arg3: int = 100,
     ) -> None:
         _ = arg1
+        _ = arg3
         original_obj = container.service()
         assert arg2.a != original_obj.a
         assert arg2.b != original_obj.b
@@ -32,3 +36,17 @@ async def test_injection_with_args_overriding_async(container) -> None:
     await _inner(True, container.service(b="url", a=2000))  # noqa: FBT003
     await _inner(arg1=True, arg2=container.service(b="url", a=2000))
     await _inner(True, arg2=container.service(b="url", a=2000))  # noqa: FBT003
+
+
+async def test_injection_async(container) -> None:
+    @inject
+    async def _inner(
+        arg1: bool,  # noqa: FBT001
+        arg2: Service = Provide[container.service],
+        arg3: int = 100,
+    ) -> None:
+        assert arg1
+        assert isinstance(arg2, Service)
+        assert arg3 == 100
+
+    await _inner(True)  # noqa: FBT003
